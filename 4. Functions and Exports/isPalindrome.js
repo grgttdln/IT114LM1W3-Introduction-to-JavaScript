288b86d6958d37335b56d4dad47af929cccd75d2
Georgette Dalen <georgette@Georgettes-MacBook-Pro.local>
2024-01-16 08:26:40 +0800

    TODO 4: DONE

--- 4. Functions and Exports/isPalindrome.js
+++ 4. Functions and Exports/isPalindrome.js
@@ -1,6 +1,6 @@
 // Todo 4.6a Create a funciton called isPalindrome function which checks if the given word is a palindrome
 // What is a Palindrome? See here: https://en.wikipedia.org/wiki/Palindrome. Feel free to search for the implementation
-export default function isPalindrome(word) {
+function isPalindrome(word) {
     const lowercase = word.toLowerCase();
     
     const reverseWord = lowercase.split('').reverse().join('');
@@ -12,3 +12,4 @@ export default function isPalindrome(word) {
 
 // Todo 4.6b Set the isPalindrome function as the default export of the module
 // Useful Tutorial: https://youtu.be/qgRUr-YUk1Q?si=ceAaeHJfCYCmeNWx
+export default isPalindrome;
